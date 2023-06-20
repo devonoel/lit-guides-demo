7dc4a2197b1e8fb429952cfaa80a79448964ee48
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2023-06-20 16:44:23 -0400

    Render skeleton react view on homepage

---
+++ app/javascript/bundles/LitGuidesDemo/components/LitGuidesDemo.jsx
@@ -0,0 +1,14 @@
+// import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import style from './LitGuidesDemo.module.css';
+
+const LitGuidesDemo = (props) => {
+  return (
+    <React.Fragment>
+      <h1>Lit Guides Demo</h1>
+    </React.Fragment>
+  );
+}
+
+export default LitGuidesDemo;
+
--- app/javascript/packs/lit-guides-demo-bundle.js
+++ app/javascript/packs/lit-guides-demo-bundle.js
@@ -1,10 +1,8 @@
 import ReactOnRails from 'react-on-rails';
 
-import HelloWorld from '../bundles/HelloWorld/components/HelloWorldServer';
 import LitGuidesDemo from '../bundles/LitGuidesDemo/components/LitGuidesDemo';
 
-// This is how react_on_rails can see the HelloWorld in the browser.
+// This is how react_on_rails can see the LitGuidesDemo in the browser.
 ReactOnRails.register({
-  HelloWorld,
   LitGuidesDemo,
 });
