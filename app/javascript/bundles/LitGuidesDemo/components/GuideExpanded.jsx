348341d7ef87a80fca66be15ea5096bb25adfdc2
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2023-06-21 15:29:07 -0400

    Get some basic react router routes going

---
+++ app/javascript/bundles/LitGuidesDemo/components/GuideExpanded.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GuideExpanded = (props) => {
+  return (
+    <React.Fragment>
+      <p>Foo</p>
+    </React.Fragment>
+  );
+}
+
+export default GuideExpanded;
--- app/javascript/bundles/LitGuidesDemo/components/LitGuidesDemo.jsx
+++ app/javascript/bundles/LitGuidesDemo/components/LitGuidesDemo.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import style from './LitGuidesDemo.module.css';
 import GuidesList from './GuidesList.jsx';
+import GuideExpanded from './GuideExpanded.jsx';
 
 const LitGuidesDemo = (props) => {
   return (
     <React.Fragment>
       <h1>Lit Guides Demo</h1>
-      <GuidesList guides={props.guides}/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<GuidesList guides={props.guides} />} exact />
+          <Route path="/foo" element={<GuideExpanded />} exact />
+        </Routes>
+      </BrowserRouter>
     </React.Fragment>
   );
 }
