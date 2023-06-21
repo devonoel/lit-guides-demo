7b8415fc117cb2a6839ae5d3d43818cb42387178
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2023-06-21 11:23:20 -0400

    Add sort buttons

--- app/javascript/bundles/LitGuidesDemo/components/LitGuidesDemo.jsx
+++ app/javascript/bundles/LitGuidesDemo/components/LitGuidesDemo.jsx
@@ -3,12 +3,31 @@ import React, { useState } from 'react';
 import style from './LitGuidesDemo.module.css';
 
 const LitGuidesDemo = (props) => {
+  const [selectedSort, setSelectedSort] = useState('title');
+
+  const sortGuides = (g1, g2) => {
+    let attr = selectedSort == 'title' ? 'name' : 'created_at';
+    return g1[attr] < g2[attr] ? -1 : g1[attr] > g2[attr] ? 1 : 0
+  }
+
   return (
     <React.Fragment>
       <h1>Lit Guides Demo</h1>
 
+      <div>
+        <button onClick={ e => setSelectedSort('title') }
+          className={selectedSort == 'title' ? style.active : ''}>
+          Title A-Z</button>
+        <button onClick={ e => setSelectedSort('recent') }
+          className={selectedSort == 'recent' ? style.active : ''}>
+          Recently Added</button>
+      </div>
+
+      <p>Selected: { selectedSort }</p>
+
       <ul>
-        { props.guides.map(g => (<li key={g.id}>{g.name}</li>)) }
+        { props.guides.sort(sortGuides)
+          .map(g => (<li key={g.id}>{g.name}</li>)) }
       </ul>
     </React.Fragment>
   );
