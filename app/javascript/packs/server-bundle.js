2a478db6bded991f0201bff4fb901c7a07770d42
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2023-06-22 00:39:01 -0400

    cleanup some unused generator code

--- app/javascript/packs/server-bundle.js
+++ app/javascript/packs/server-bundle.js
@@ -1,10 +1,6 @@
 import ReactOnRails from 'react-on-rails';
-
-import HelloWorld from '../bundles/HelloWorld/components/HelloWorldServer';
 import LitGuidesDemo from '../bundles/LitGuidesDemo/components/LitGuidesDemo';
 
-// This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
-  HelloWorld,
   LitGuidesDemo,
 });
